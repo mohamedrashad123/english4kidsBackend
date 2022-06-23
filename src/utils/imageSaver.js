cee9849c86c3a285e277e930b6c9f48bbe513ecd
mohamedrashad123 <mohamedrashad@dipdux.com>
2022-06-23 08:44:49 +0200

    upload files

--- src/utils/imageSaver.js
+++ src/utils/imageSaver.js
@@ -2,48 +2,40 @@ const fs = require("fs");
 const path = require("path");
 
 exports.getImgPath = (img, folder) => {
-    try {
-        // get upload image name
-        const uploadImgName = img.path.slice(
-            img.path.lastIndexOf(
-                `${process.platform === "win32" ? "\\" : "/"}`
-            ) + 1
+    // get upload image name
+    const uploadImgName = img.path.slice(
+        img.path.lastIndexOf(`${process.platform === "win32" ? "\\" : "/"}`) + 1
+    );
+
+    // get img full name
+    const imgFullName = img.name;
+
+    // get image extention
+    const extention = imgFullName.slice(imgFullName.indexOf("."));
+
+    // create new image name
+    const newImgName = `${Math.floor(Math.random() * Date.now()) + extention}`;
+
+    // save image to new path
+    const newPath = path.resolve(
+        __dirname,
+        `../../../cdn-english4kids/${folder}/${newImgName}`
+    );
+
+    // get uploaded image data
+    fs.readFile(img.path, (err, data) => {
+        // create new image
+        fs.writeFile(newPath, data, (err) => {});
+
+        // delete uploaded image
+        fs.unlink(
+            path.resolve(__dirname, `../public/${uploadImgName}`),
+            (err) => {
+                if (err) console.log(err);
+                console.log("file has been deleted");
+            }
         );
+    });
 
-        // get img full name
-        const imgFullName = img.name;
-
-        // get image extention
-        const extention = imgFullName.slice(imgFullName.indexOf("."));
-
-        // create new image name
-        const newImgName = `${
-            Math.floor(Math.random() * Date.now()) + extention
-        }`;
-
-        // save image to new path
-        const newPath = path.resolve(
-            __dirname,
-            `../../../cdn/${folder}/${newImgName}`
-        );
-
-        // get uploaded image data
-        fs.readFile(img.path, (err, data) => {
-            // create new image
-            fs.writeFile(newPath, data, (err) => {});
-
-            // delete uploaded image
-            fs.unlink(
-                path.resolve(__dirname, `../public/${uploadImgName}`),
-                (err) => {
-                    if (err) console.log(err);
-                    console.log("file has been deleted");
-                }
-            );
-        });
-
-        return newImgName;
-    } catch (err) {
-        throw err;
-    }
+    return newImgName;
 };
